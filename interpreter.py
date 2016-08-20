77ba47749cb8deb091d5e2b7e0c4e950e0e84590
Frederick Wagner <doctaphred@gmail.com>
2016-08-19 22:55:03 -0400

    Improve some names

--- interpreter.py
+++ interpreter.py
@@ -7,6 +7,7 @@ from stack import Stack
 from utils import struct
 
 
+# TODO: capture stdin, stdout, stderr
 Record = struct('statement', 'result')
 
 
@@ -15,109 +16,109 @@ class Stark:
     def __init__(self, hist_length=1000):
         self.hist = deque(maxlen=hist_length)
         self.stack = Stack({
-            name[6:]: getattr(self, name)
+            name[4:]: getattr(self, name)
             for name in dir(self)
-            if name.startswith('stark_')
+            if name.startswith('cmd_')
         })
 
-    def stark_eval(self, statement):
+    def cmd_eval(self, statement):
         name, *args = statement
         try:
-            res = self.stack[name](*args)
+            result = self.stack[name](*args)
         except Exception as ex:
             traceback.print_exc()
-            res = ex
-        self.hist.append(Record(statement, res))
-        return res
+            result = ex
+        self.hist.append(Record(statement, result))
+        return result
 
-    def stark_exec(self, code):
+    def cmd_exec(self, code):
         program = lex(code)
         # TODO: handle empty program better (return sentinel value?
-        # don't update self.res?)
+        # don't update self.result?)
         assert program
         for statement in program:
-            res = self.stark_eval(statement)
-        return res
+            result = self.cmd_eval(statement)
+        return result
 
-    def stark_push(self):
+    def cmd_push(self):
         self.stack.push()
 
-    def stark_pop(self):
+    def cmd_pop(self):
         return self.stack.pop()
 
-    def stark_res(self, *args):
+    def cmd_result(self, *args):
         index, = args
         return self.hist[-int(index)].result
 
-    def stark_hist(self, *args):
+    def cmd_hist(self, *args):
         index, = args
         return self.hist[-int(index)].statement
 
-    def stark_set(self, *args):
+    def cmd_set(self, *args):
         name, *args = args
-        self.stack[name] = self.stark_eval(args)
+        self.stack[name] = self.cmd_eval(args)
         return None
 
-    def stark_int(self, *args):
+    def cmd_int(self, *args):
         i, = args
         return int(i)
 
-    def stark_get(self, *args):
+    def cmd_get(self, *args):
         name, = args
         return self.stack[name]
 
-    def stark_getall(self, *args):
+    def cmd_getall(self, *args):
         return tuple(self.stack[a] for a in args)
 
-    def stark_del(self, *args):
+    def cmd_del(self, *args):
         name, = args
         return self.stack.pop(name)
 
-    def stark_delall(self, *args):
+    def cmd_delall(self, *args):
         # TODO: validate names first
         return tuple(self.stack.pop(a) for a in args)
 
-    def stark_say(self, *args):
+    def cmd_say(self, *args):
         print(*args)
         return None
 
-    def stark_show(self, *args):
+    def cmd_show(self, *args):
         print(*[self.stack[a] for a in args])
         return None
 
-    def stark_while(self, *args):
+    def cmd_while(self, *args):
         cond, code = args
-        while self.stark_exec(cond):
-            self.stark_exec(code)
+        while self.cmd_exec(cond):
+            self.cmd_exec(code)
 
-    def stark_reduce(self, *args):
+    def cmd_reduce(self, *args):
         func, *args = args
         values = [self.stack[a] for a in args]
         return reduce(self.stack[func], values)
 
-    def stark_eq(self, *args):
+    def cmd_eq(self, *args):
         a, b = args
         return self.stack[a] == self.stack[b]
 
-    def stark_ne(self, *args):
+    def cmd_ne(self, *args):
         a, b = args
         return self.stack[a] != self.stack[b]
 
-    def stark_lt(self, *args):
+    def cmd_lt(self, *args):
         a, b = args
         # TODO: chain across arbitrarily many!! :D
         return self.stack[a] < self.stack[b]
 
-    def stark_sum(self, *args):
+    def cmd_sum(self, *args):
         return sum(self.stack[a] for a in args)
 
-    def stark_any(self, *args):
+    def cmd_any(self, *args):
         return any(self.stack[a] for a in args)
 
-    def stark_all(self, *args):
+    def cmd_all(self, *args):
         return all(self.stack[a] for a in args)
 
-    def stark_python(self, *args):
+    def cmd_python(self, *args):
         code, = args
         exec(code, globals())
         return None
@@ -127,13 +128,13 @@ if __name__ == '__main__':
     stark = Stark()
 
     def ev(*args):
-        stark.stark_eval(args)
+        stark.cmd_eval(args)
 
     def ex(code):
-        stark.stark_exec(code)
+        stark.cmd_exec(code)
 
     def exf(path):
         with open(path) as f:
-            stark.stark_exec(f.read())
+            stark.cmd_exec(f.read())
 
     exf('test.stark')
