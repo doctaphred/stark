f062cb9a7acb6a2d515a01877d0065f4a18d5593
Frederick Wagner <doctaphred@gmail.com>
2016-08-21 15:02:20 -0400

    Add pipe logic

---
+++ pipe.py
@@ -0,0 +1,65 @@
+PIPE = '>'
+
+
+def pipe(statement):
+    """
+
+    (forward, backward) for 'abc>def':
+    a: a, <empty>
+    b: ab, <empty>
+    c: abc, <empty>
+    >: <empty>, cba
+    d: d, cba
+    e: de, cba
+    f: def, cba
+    => defabc, <empty>
+
+    >>> pipe(['list', 'a', 'b', 'c', '>', 'set', 'x'])
+    ['set', 'x', 'list', 'a', 'b', 'c']
+
+    >>> def ppipe(s):
+    ...     print(''.join(pipe(s)))
+
+    >>> ppipe('abc')
+    abc
+    >>> ppipe('abc>def')
+    defabc
+    >>> ppipe('abc>def>ghi')
+    ghidefabc
+
+    >>> ppipe('abc>>def')
+    defabc
+    >>> ppipe('abc>>>>>>>>>>def')
+    defabc
+
+    >>> ppipe('abc>')
+    abc
+    >>> ppipe('>abc')
+    abc
+    >>> ppipe('>abc>def>ghi>')
+    ghidefabc
+
+    >>> ppipe('a>b>c>d>e>f')
+    fedcba
+    >>> ppipe('>a>b>c>d>e>f>')
+    fedcba
+    """
+    forward = []
+    backward = []
+
+    def flip():
+        while forward:
+            backward.append(forward.pop())
+
+    def unflip():
+        while backward:
+            forward.append(backward.pop())
+
+    for token in statement:
+        if token == PIPE:
+            flip()
+        else:
+            forward.append(token)
+
+    unflip()
+    return forward
